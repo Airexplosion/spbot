d68664168617767316be806108184c393336227c
Airexplosion <zd1179589071@163.com>
2022-06-04 17:50:25 +0800

    尝试写了禁言相关接口的调用
    结果不行 哈哈

--- Bot-main.py
+++ Bot-main.py
@@ -12,7 +12,7 @@ rem = 5
 async def _(event: Event):  # M3
     global rem, mag
     msg = event['message']
-    if msg.startswith('/'):
+    if msg.startswith('.'):
         tmsg = event['message'][1:]
         await bot.send(event, '你发了：' + tmsg)  # M4
     elif msg == '俄罗斯轮盘赌':
@@ -21,11 +21,17 @@ async def _(event: Event):  # M3
         re = mag[int(bul)]
         if rem == -1:
             await bot.send(event, "你已知道，这是必死之局。")
+            # ban = random.randint(60, 300)
+            # await bot.set_group_ban(int(ban))
+            # await bot.send(event, "砰，你死了。距离下次复活还有：" + str(int(ban)) + "分")
             await bot.send(event, "砰，你死了。")
             await bot.send(event, "已重新装填子弹。")
             mag = [1, 2, 3, 4, 5, 6]
             rem = 5
         elif re == 6:
+            # ban = random.randint(60, 300)
+            # await bot.set_group_ban(int(ban))
+            # await bot.send(event, "砰，你死了。距离下次复活还有：" + str(int(ban)) + "分")
             await bot.send(event, "砰，你死了。")
             await bot.send(event, "已重新装填子弹。")
             mag = [1, 2, 3, 4, 5, 6]
@@ -35,6 +41,9 @@ async def _(event: Event):  # M3
             await bot.send(event, "“咔嚓”")
             await bot.send(event, "哈，是空的。")
             await bot.send(event, "侥幸逃过一劫呢，哼。剩余弹仓：" + str(len(mag)))
+    elif msg == '/占卜':
+        await bot.send(event, "占你妈个头，滚！")
+
 
 #暂时这样！
 bot.run(host='127.0.0.1', port=8080)  # M6
